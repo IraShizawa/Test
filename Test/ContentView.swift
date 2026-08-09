273e5499102674a38adba5a364014b9bf451a64b
Ira-Shizawa <zhizedawei069@gmail.com>
2026-08-09 17:33:32 +0900

    Initial Commit

---
+++ Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Test
+//
+//  Created by Shizawa Ira on 2026/08/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Shizawa Ira on 2026/08/09.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
